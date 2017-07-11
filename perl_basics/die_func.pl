cbc9eb7a496418df0df5291765303ca530e7a744
Yann Yu <yann.yu@medlytix.com>
2017-07-11 14:45:01 -0400

    perl basics refresher

---
+++ perl_basics/die_func.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+die "No greeting possible at this time; stopped";
---
+++ perl_basics/warn_func.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+warn "It may or may not greet you.\n";
+print "Let's try it anyway!\n"
