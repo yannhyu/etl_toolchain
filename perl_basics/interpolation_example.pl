3945486d887ec51bec6bc5f63513cc0a493a88a7
Yann Yu <yann.yu@medlytix.com>
2017-07-12 10:45:46 -0400

    add interpolation and scalar examples

---
+++ perl_basics/interpolation_example.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $count = 14;
+my $fruit = "apple";
+
+print "I've got $count ${fruit}s\n";
---
+++ perl_basics/scalar_examples.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $PI = 3.14159;
+print "Pi is ", $PI, "\n";
+
+my $radius = 1_500_000;
+print "Radius is ", $radius, "\n";
