75f9e4a3713bba7bf7d29f9e9de5a12100709bd6
Yann Yu <yann.yu@medlytix.com>
2017-07-14 13:49:41 -0400

    add more tools

---
+++ sqlalchemy_tools/get_tbl_col_info_from_db/step_05_get_columns_details.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy import Table
+
+db_uri = "postgresql://u:p@host/database"
+engine = create_engine(db_uri)
+
+# Create MetaData instance
+metadata = MetaData(engine, reflect=True)
+# print(metadata.tables)
+
+# Get Table
+ex_table = metadata.tables['temp_batch_t']
+print(ex_table)
+
+# User.name.property.columns[0].type.length
+# print user.columns.name.type.length
+
+for column in ex_table.columns:
+    try:
+        print('{}: {} with length {}'.format(column.name, column.type, column.type.length))
+    except AttributeError as ae:
+        # print("{} - 'NUMERIC' object has no attribute 'length'".format(column.name))
+        print('{}: NUMERIC'.format(column.name))
