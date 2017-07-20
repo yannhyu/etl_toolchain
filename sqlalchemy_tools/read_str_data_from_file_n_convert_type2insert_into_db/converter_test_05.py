ab63130f172be5d89c5a1a2cbfae2b0209952d3f
Yann Yu <yann.yu@medlytix.com>
2017-07-20 12:14:26 -0400

    bug fixes

--- sqlalchemy_tools/read_str_data_from_file_n_convert_type2insert_into_db/converter_test_05.py
+++ sqlalchemy_tools/read_str_data_from_file_n_convert_type2insert_into_db/converter_test_05.py
@@ -8,7 +8,7 @@ from sqlalchemy import Table
 from sqlalchemy.dialects.postgresql import \
     CHAR, VARCHAR, DATE, TIMESTAMP, INTEGER, BIGINT, NUMERIC
 
-db_uri = "postgresql://u:p@host/database"
+# db_uri = "postgresql://u:p@host/database"
 db_uri = 'postgresql://dev_payorintel_u:u782nmt5@devmaindb/dev_ml_db'
 engine = create_engine(db_uri)
 
@@ -60,15 +60,23 @@ def generate_data_type_map():
 # handle data type conversion in each data_line
 def convert_data_type(data_line):
     fld_type_map = generate_data_type_map()
-    return {k:fld_type_map[k](v) for k, v in data_line.iteritems()}
+    yield {k:fld_type_map[k](v) for k, v in data_line.iteritems()}
+
+def post_type_conversion_add(data_line):
+    data_line = next(data_line)
+    data_line['our_run_date'] = datetime.now()
+    yield data_line    
 
 v4_headers = tuple(generate_headers('read_insurance0_v4_t'))
 # print(v4_headers)
 data_lines = generate_data_line(v4_headers, 'Data/reader310.output')
 
-for data_line in data_lines:
-    print(data_line)
-    print(convert_data_type(data_line))
+# for data_line in data_lines:
+#     print(data_line)
+#     print(convert_data_type(data_line))
 
+data_lines_typed = [convert_data_type(data_line) for data_line in data_lines]
+data_rows = [post_type_conversion_add(data_line) for data_line in data_lines_typed]
 
-
+for row in data_rows:
+    print(next(row))
