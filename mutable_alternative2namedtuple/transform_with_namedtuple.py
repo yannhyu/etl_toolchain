6b1cd6f00f4eae57d4a6a9e86bac7eb69d8244a5
yannhyu <yannhyu@yahoo.com>
2017-07-23 18:09:03 -0400

    Create transform_with_namedtuple.py

---
+++ mutable_alternative2namedtuple/transform_with_namedtuple.py
@@ -0,0 +1,32 @@
+from collections import namedtuple
+
+master_headers = ('fname', 'lname', 'sex', 'age', 'bday', 'special')
+
+blank_master = (None,) * len(master_headers)
+
+Row = namedtuple('Row', master_headers)
+
+empty = Row(*blank_master)
+# print(empty)
+
+headers_v1 = ('lname', 'bday', 'special')
+data_v1 = [
+        ('Roberts', '2000-03-12', 'opera'),
+        ('Ashford', '1995-07-15', 'books'),
+        ('Whatitis', '2017-01-03', 'music'),
+    ]
+
+headers_v2 = ('fname', 'lname', 'special', 'age')
+data_v2 = [
+        ('David', 'Roberts', 'ping pong', 36),
+        ('Carlo', 'Ashford', 'python', 35),
+        ('Howabout', 'Whatitis', 'music', 3),
+    ]
+
+for d_v1 in data_v1:
+    transformed = empty._replace(**dict(zip(headers_v1, d_v1)))
+    print(transformed)
+
+for d_v2 in data_v2:
+    transformed = empty._replace(**dict(zip(headers_v2, d_v2)))
+    print(transformed)
